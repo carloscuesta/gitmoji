abbdba4b62cb3893defa14a59a8839af3bc9652d
Carlos Cuesta <carloscuesta@me.com>
2019-11-02 19:50:39 +0100

    :sparkles: Add copy to clipboard

--- src/components/GitmojiList/index.js
+++ src/components/GitmojiList/index.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react'
+import Clipboard from 'clipboard'
 
 import Gitmoji from './Gitmoji'
 
@@ -12,18 +13,39 @@ type Props = {
   }>
 }
 
-const GitmojiList = (props: Props) => (
-  <div className="row center-xs" id="gitmoji-list">
-    {props.gitmojis.map((gitmoji, index) => (
-      <Gitmoji
-        code={gitmoji.code}
-        description={gitmoji.description}
-        emoji={gitmoji.emoji}
-        key={index}
-        name={gitmoji.name}
-      />
-    ))}
-  </div>
-)
+const GitmojiList = (props: Props) => {
+  React.useEffect(() => {
+    const clipboard = new Clipboard('.gitmoji')
+
+    clipboard.on('success', function() {
+      window.ga('send', 'event', 'Gitmoji', 'Copy to Clipboard')
+
+      var notification = new window.NotificationFx({
+        message: '<p>Hey! Gitmoji copied to the clipboard 😜</p>',
+        layout: 'growl',
+        effect: 'scale',
+        type: 'notice'
+      })
+
+      notification.show()
+    })
+
+    return () => clipboard.destroy()
+  }, [])
+
+  return (
+    <div className="row center-xs" id="gitmoji-list">
+      {props.gitmojis.map((gitmoji, index) => (
+        <Gitmoji
+          code={gitmoji.code}
+          description={gitmoji.description}
+          emoji={gitmoji.emoji}
+          key={index}
+          name={gitmoji.name}
+        />
+      ))}
+    </div>
+  )
+}
 
 export default GitmojiList
