a5dd85ef479243dbac04553b8ccc591905f6950f
Benjamin Caradeuc <bencrdc@gmail.com>
2020-04-27 15:44:16 +0200

    :lipstick: Add dark-mode for prefers-color-scheme: dark
    Will make my eyes happy when comming from my text editor

--- src/components/Icon/definitions.js
+++ src/components/Icon/definitions.js
@@ -16,7 +16,7 @@ export const IconDefinitions = () => (
         <path
           className="heart"
           d="m61.1 18.2c-6.4-17-27.2-9.4-29.1-.9-2.6-9-22.9-15.7-29.1.9-6.9 18.5 26.7 35.1 29.1 37.8 2.4-2.2 36-19.6 29.1-37.8"
-          fill="#ff5a79"
+          fill="currentcolor"
         />
       </symbol>
       <symbol id="icon-star" viewBox="0 0 64 64">
