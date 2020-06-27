d471ee303d47ce9d9f2c45945eca03a0647bf40d
johannchopin <johannchopin@pm.me>
2020-06-27 16:15:04 +0200

    🚨 Fix layoutName flow type

--- src/components/Button/ToggleLayoutButtons.js
+++ src/components/Button/ToggleLayoutButtons.js
@@ -3,10 +3,11 @@ import React from 'react'
 
 import Icon from '../Icon'
 
-type Props = { layout: 'list' | 'grid', onClick: () => void }
+type LayoutName = 'list' | 'grid'
+type Props = { layout: LayoutName, onClick: (layoutName: LayoutName) => void }
 
 const ToggleLayoutButtons = (props: Props) => {
-  const renderLayoutBtn = (layoutName) => (
+  const renderLayoutBtn = (layoutName: LayoutName) => (
     <button className={`btn layout ${props.layout === layoutName ? 'selected': ''}`} onClick={() => { props.onClick(layoutName) }}>
       <Icon name={layoutName} />
     </button>
