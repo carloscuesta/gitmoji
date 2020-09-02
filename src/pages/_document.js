c5bc37991a5d053c278505cad6d60922b22595a8
Carlos Cuesta <carloscuesta@me.com>
2020-09-02 09:11:29 +0200

    :fire: Remove InlineStylesHead

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,42 +4,11 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { readFileSync } from 'fs'
 import { join } from 'path'
 
-class InlineStylesHead extends Head {
-  getCssLinks() {
-    return this.__getInlineStyles()
-  }
-
-  __getInlineStyles() {
-    const { assetPrefix, files } = this.context
-    if (!files || files.length === 0) return null
-
-    return files
-      .filter((file) => /\.css$/.test(file))
-      .map((file) => (
-        <style
-          key={file}
-          nonce={this.props.nonce}
-          data-href={`${assetPrefix}/_next/${file}`}
-          dangerouslySetInnerHTML={{
-            __html: readFileSync(join(process.cwd(), '.next', file), 'utf-8'),
-          }}
-        />
-      ))
-  }
-}
-
-/*
-  This code has been obtained from:
-  https://github.com/zeit/next-plugins/issues/238#issuecomment-432211871
-*/
-
 class CustomDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <InlineStylesHead />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
