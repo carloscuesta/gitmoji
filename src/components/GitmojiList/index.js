ca8951b74e39cd17d5d86de3c55897d9b5eae0d8
ilyasmez <ilyas@amezouar.com>
2020-12-08 18:15:19 +0100

    :ok_hand: Refactor duplicate .toLowercase, as suggested by @johannchopin

--- src/components/GitmojiList/index.js
+++ src/components/GitmojiList/index.js
@@ -18,11 +18,14 @@ type Props = {
 const GitmojiList = (props: Props): Element<'div'> => {
   const [searchInput, setSearchInput] = React.useState(null)
   const gitmojis = searchInput
-    ? props.gitmojis.filter(
-        ({ code, description }) =>
-          code.includes(searchInput.toLowerCase()) ||
-          description.toLowerCase().includes(searchInput.toLowerCase())
-      )
+    ? props.gitmojis.filter(({ code, description }) => {
+        const lowerCasedSearch = searchInput.toLowerCase()
+
+        return (
+          code.includes(lowerCasedSearch) ||
+          description.toLowerCase().includes(lowerCasedSearch)
+        )
+      })
     : props.gitmojis
 
   React.useEffect(() => {
