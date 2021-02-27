bdfa47dd2cbdf6bd267877a8836483ee54e905d2
Carlos Cuesta <carloscuesta@me.com>
2021-02-27 17:24:07 +0100

    :sparkles: Introduce gitmoji API

--- src/__tests__/stubs.js
+++ src/__tests__/stubs.js
@@ -2,3 +2,17 @@ export const appProps = {
   Component: (props) => <div {...props}>Component</div>,
   pageProps: { test: '' },
 }
+
+export const request = (method) => ({ method })
+
+export const response = () => {
+  const response = {}
+
+  response.status = jest.fn().mockReturnValue(response)
+  response.json = jest.fn().mockReturnValue(response)
+  response.setHeader = jest.fn().mockReturnValue(response)
+  response.status = jest.fn().mockReturnValue(response)
+  response.end = jest.fn().mockReturnValue(response)
+
+  return response
+}
