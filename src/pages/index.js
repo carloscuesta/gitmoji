813a23c3d173c99abb607d2e66e1da17054d8ae5
Carlos Cuesta <carloscuesta@me.com>
2021-06-15 23:13:25 +0200

    ⬆️ Bump Next.js to 11 (#799)
    * :arrow_up: Bump Next.js to 11
    * :bug: Fix import warning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 
-import { gitmojis } from 'src/data/gitmojis.json'
+import gitmojis from 'src/data/gitmojis.json'
 import GitmojiList from 'src/components/GitmojiList'
 import CarbonAd from 'src/components/CarbonAd'
 import SEO from 'src/components/SEO'
@@ -11,7 +11,7 @@ const Home = () => (
     <SEO />
     <main>
       <CarbonAd />
-      <GitmojiList gitmojis={gitmojis} />
+      <GitmojiList gitmojis={gitmojis.gitmojis} />
     </main>
   </>
 )
