48042f405e2cd4d7ecb3ba753afe5291a8078636
Carlos Cuesta <carloscuesta@me.com>
2021-08-01 11:55:08 +0200

    :sparkles: Fetch Contributors using `getStaticProps` (#828)

--- src/components/ContributorsList/Contributor/index.js
+++ src/components/ContributorsList/Contributor/index.js
@@ -3,11 +3,11 @@ import React, { type Element } from 'react'
 
 import styles from './styles.module.css'
 
-type Props = { profile: string, avatar: string }
+type Props = { avatar: string, url: string }
 
 const Contributor = (props: Props): Element<'article'> => (
   <article className="col-xs-3 col-sm-2">
-    <a href={props.profile}>
+    <a href={props.url} target="_blank">
       <img className={styles.picture} src={props.avatar} />
     </a>
   </article>
--- src/components/ContributorsList/__tests__/stubs.js
+++ src/components/ContributorsList/__tests__/stubs.js
@@ -1,4 +1,9 @@
-export const props = {
-  profile: 'https://github.com/profile',
+export const contributor = {
+  url: 'https://github.com/profile',
   avatar: 'https://github.com/avatar',
+  id: 'contributor-id-123',
+}
+
+export const props = {
+  contributors: [contributor],
 }
