c0e86da60699feda921df7854e1171486adc1f0a
Max Programming <51731966+max-programming@users.noreply.github.com>
2021-08-02 18:35:00 +0530

    ✨ Introduce gitmoji PWA with `next-pwa` (#825)

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
+  reactStrictMode: true,
+})
