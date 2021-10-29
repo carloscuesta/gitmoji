bb7bc4d1be9fc54a37eee9fd8b112ff753c1e1c1
r4z0rFr <erwankreutz@gmail.com>
2021-10-30 01:18:43 +0200

    🎨 Add useRef to remove event

--- src/components/GitmojiList/Toolbar/index.js
+++ src/components/GitmojiList/Toolbar/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { type Element } from 'react'
+import React, { type Element, useEffect, createRef } from 'react'
 
 import ListModeSelector from './ListModeSelector'
 import styles from './styles.module.css'
@@ -11,36 +11,52 @@ type Props = {
   setSearchInput: Function,
 }
 
-if (typeof window !== 'undefined') {
-  document.addEventListener('keydown', (event: KeyboardEvent) => {
-    const searchInput = document.querySelector('#searchInput')
-    if (searchInput && (event.ctrlKey || event.metaKey) && event.key === 'k') {
-      searchInput.focus()
-    }
-  })
+const searchInputRef = createRef()
+const keyboardEventListener = (event: KeyboardEvent) => {
+  const searchInput = searchInputRef.current
+  if (searchInput && (event.ctrlKey || event.metaKey) && event.key === 'k') {
+    event.preventDefault()
+    searchInput.focus()
+  }
 }
 
-const Toolbar = (props: Props): Element<'div'> => (
-  <div className={styles.container}>
-    <div className={styles.inputWrapper}>
-      <input
-        className={styles.searchInput}
-        id="searchInput"
-        name="searchInput"
-        onChange={(event) => props.setSearchInput(event.target.value)}
-        placeholder="Search your gitmoji..."
-        type="text"
-        value={props.searchInput}
+const isMacOs =
+  typeof window !== 'undefined' &&
+  window.navigator.platform.toUpperCase().indexOf('MAC') >= 0
+
+const Toolbar = (props: Props): Element<'div'> => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      document.addEventListener('keydown', keyboardEventListener, false)
+    }
+
+    return () => {
+      document.removeEventListener('keydown', keyboardEventListener, false)
+    }
+  })
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.inputWrapper}>
+        <input
+          className={styles.searchInput}
+          ref={searchInputRef}
+          name="searchInput"
+          onChange={(event) => props.setSearchInput(event.target.value)}
+          placeholder="Search your gitmoji..."
+          type="text"
+          value={props.searchInput}
+        />
+
+        <kbd className={styles.kbd}>{isMacOs ? '⌘' : 'Ctrl'} K</kbd>
+      </div>
+
+      <ListModeSelector
+        isListMode={props.isListMode}
+        setIsListMode={props.setIsListMode}
       />
-
-      <kbd className={styles.kbd}>⌘K</kbd>
     </div>
-
-    <ListModeSelector
-      isListMode={props.isListMode}
-      setIsListMode={props.setIsListMode}
-    />
-  </div>
-)
+  )
+}
 
 export default Toolbar
