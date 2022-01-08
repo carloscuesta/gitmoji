a6abca8e797075ff33d8193f0c0ae9dd3b38862a
johannchopin <johannchopin@pm.me>
2022-01-08 12:01:01 +0100

    ♻️ Use currentColor to fill svg

--- src/components/Icon/definitions.js
+++ src/components/Icon/definitions.js
@@ -24,7 +24,7 @@ export const IconDefinitions = (): Element<'svg'> => (
         <path
           className="twitter"
           d="M62,25.2H39.1L32,3l-7.1,22.2H2l18.5,13.7l-7,22.1L32,47.3L50.5,61l-7.1-22.2L62,25.2z"
-          fill="#FFDD67"
+          fill="currentColor"
         />
       </symbol>
       <symbol id="icon-twitter" viewBox="0 0 64 64">
