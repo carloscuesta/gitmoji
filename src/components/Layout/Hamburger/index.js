15ccc71c4a676a80f756062e4cb4c4234575b363
johannchopin <31794680+johannchopin@users.noreply.github.com>
2022-01-09 12:04:01 +0100

    ♿️ Add focus trap to the menu (#955)

--- src/components/Layout/Hamburger/index.js
+++ src/components/Layout/Hamburger/index.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { type Element } from 'react'
 import Router from 'next/router'
+import FocusTrap from 'focus-trap-react'
 
 import MenuLink from './MenuLink'
 import OpenIcon from './OpenIcon'
@@ -31,38 +32,40 @@ const Hamburger = (): Element<'div'> => {
       </button>
 
       {isOpen && (
-        <nav className={styles.menu}>
-          <div className={styles.closeContainer}>
-            <button
-              aria-label="Close navigation menu"
-              className={styles.button}
-              onClick={() => setIsOpen(false)}
-            >
-              <CloseIcon />
-            </button>
-          </div>
+        <FocusTrap active={isOpen}>
+          <nav className={styles.menu}>
+            <div className={styles.closeContainer}>
+              <button
+                aria-label="Close navigation menu"
+                className={styles.button}
+                onClick={() => setIsOpen(false)}
+              >
+                <CloseIcon />
+              </button>
+            </div>
 
-          <ul className={styles.links}>
-            <li>
-              <MenuLink href="/" text="Home" />
-            </li>
-            <li>
-              <MenuLink href="/about" text="About" />
-            </li>
-            <li>
-              <MenuLink href="/contributors" text="Contributors" />
-            </li>
-            <li>
-              <MenuLink href="/related-tools" text="Related tools" />
-            </li>
-            <li>
-              <MenuLink
-                href="https://github.com/carloscuesta/gitmoji"
-                text="GitHub"
-              />
-            </li>
-          </ul>
-        </nav>
+            <ul className={styles.links}>
+              <li>
+                <MenuLink href="/" text="Home" />
+              </li>
+              <li>
+                <MenuLink href="/about" text="About" />
+              </li>
+              <li>
+                <MenuLink href="/contributors" text="Contributors" />
+              </li>
+              <li>
+                <MenuLink href="/related-tools" text="Related tools" />
+              </li>
+              <li>
+                <MenuLink
+                  href="https://github.com/carloscuesta/gitmoji"
+                  text="GitHub"
+                />
+              </li>
+            </ul>
+          </nav>
+        </FocusTrap>
       )}
     </div>
   )
