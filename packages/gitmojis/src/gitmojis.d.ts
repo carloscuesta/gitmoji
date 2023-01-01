328b4006730ca5dd1f9dac0423268437721bd448
Carlos Cuesta <carloscuesta@me.com>
2023-01-01 13:48:00 +0100

    🏷️ Add `gitmojis.d.ts` TypeScript module declaration

---
+++ packages/gitmojis/src/gitmojis.d.ts
@@ -0,0 +1,34 @@
+declare module 'gitmojis' {
+  type Gitmoji = {
+    /**
+     * Gitmoji unicode character
+     * @example '🎨', '⚡️', '🔥', '🐛'
+     */
+    readonly emoji: string;
+    /**
+     * Gitmoji hexadecimal entity.
+     * @example '&#x1f3a8;', '&#x26a1;', '&#x1f525;', '&#x1f41b;'
+     */
+    readonly entity: `&#${string};`;
+    /**
+     * Gitmoji use-case description.
+     */
+    readonly description: string;
+    /**
+     * Gitmoji name.
+     * @example 'art', 'zap', 'fire', 'bug'
+     */
+    readonly name: string;
+    /**
+     * Gitmoji semver range. Can be `null` if not specified.
+     */
+    readonly semver: 'patch' | 'minor' | 'major' | null;
+    /**
+     * Gitmoji character formatted as a shortcode.
+     * @example ':art:', ':zap:', ':fire:', ':bug:'
+     */
+    readonly code: `:${string}:`;
+  }
+
+  export const gitmojis: readonly Gitmoji[];
+}
