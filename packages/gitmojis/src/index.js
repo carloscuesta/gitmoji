6b4c969510394703ce8c5e834daa282f93dd3dc8
Carlos Cuesta <carloscuesta@me.com>
2023-01-17 23:14:10 +0100

    ✨ Add `assert { type: json }` for JSON imports

--- packages/gitmojis/src/index.js
+++ packages/gitmojis/src/index.js
@@ -1,5 +1,5 @@
-import gitmojisJson from './gitmojis.json'
+import gitmojisJson from './gitmojis.json' assert { type: 'json' }
 
-export { default as schema } from './schema.json'
+export { default as schema } from './schema.json' assert { type: 'json' }
 
 export const gitmojis = gitmojisJson.gitmojis
