d9759e51c19e4454eef95391791d46978e53050e
Carlos Cuesta <carloscuesta@me.com>
2023-01-17 22:52:57 +0100

    ✨ Enable `vercel/analytics` in website

--- packages/website/src/pages/_app.tsx
+++ packages/website/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react'
 
 import Layout from 'src/components/Layout'
 import 'src/utils/theme/theme.css'
@@ -8,6 +9,7 @@ const App = (props: AppProps) => (
   <ThemeProvider>
     <Layout>
       <props.Component {...props.pageProps} />
+      <Analytics />
     </Layout>
   </ThemeProvider>
 )
