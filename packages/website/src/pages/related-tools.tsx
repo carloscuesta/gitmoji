ab6aa84a72083fa024d382cbec7e6105d5439f5e
Maja Abramski-Kronenberg <foo@digitalwords.net>
2024-10-07 21:48:32 +0300

    📝 Alphabetise the related tools page; remove randomisation
    https://github.com/carloscuesta/gitmoji/pull/1816#issuecomment-2397396059

--- packages/website/src/pages/related-tools.tsx
+++ packages/website/src/pages/related-tools.tsx
@@ -12,11 +12,6 @@ const tools = [
 				link: 'https://github.com/TimoZacherl/gitimoji',
 			},
 			{
-				name: 'traymoji',
-				description: 'A Electron Tray App for Gitmojis',
-				link: 'https://github.com/CoenWarmer/traymoji',
-			},
-			{
 				name: 'gitmojiapp',
 				description: 'A Flutter Gitmoji App for macOS, Linux, and Windows',
 				link: 'https://github.com/patrick-fu/GitmojiApp',
@@ -26,12 +21,22 @@ const tools = [
 				description: 'MacOS menubar for easy copy gitmoji',
 				link: 'https://github.com/techinpark/gitmojiBar',
 			},
+			{
+				name: 'traymoji',
+				description: 'A Electron Tray App for Gitmojis',
+				link: 'https://github.com/CoenWarmer/traymoji',
+			},
 		],
 	},
 	{
 		emoji: '🏭',
 		title: 'Automatic generators',
 		items: [
+			{
+				name: 'genmoji',
+				description: 'Gitmoji commit message generation using AI',
+				link: 'https://github.com/segersniels/genmoji',
+			},
 			{
 				name: 'gitmoji-changelog',
 				description: 'A changelog generator for gitmoji',
@@ -41,11 +46,6 @@ const tools = [
 				name: 'gitmoji-changelog-action',
 				description: 'GitHub Action for gitmoji-changelog',
 				link: 'https://github.com/sercanuste/gitmoji-changelog-action',
-			},
-			{
-				name: 'genmoji',
-				description: 'Gitmoji commit message generation using AI',
-				link: 'https://github.com/segersniels/genmoji',
 			},
 			{
 				name: 'gitmoji-semver',
@@ -59,20 +59,14 @@ const tools = [
 		title: 'Browser extensions',
 		items: [
 			{
+				name: 'githubmoji',
+				description: 'A Firefox addon that adds a predictive gitmoji picker to GitHub commit message inputs',
+				link: 'https://addons.mozilla.org/firefox/addon/githubmoji',
+			},
+			{
 				name: 'gitmoji-browser-extension',
 				description: 'The Gitmoji extension to easily search and copy gitmojis',
 				link: 'https://github.com/johannchopin/gitmoji-browser-extension',
-			},
-			{
-				name: 'githubmoji',
-				description:
-				'A Firefox addon that adds a predictive gitmoji picker to GitHub commit message inputs',
-				link: 'https://github.com/ma1ted/githubmoji',
-			},
-			{
-				name: 'githubmoji',
-				description: 'A Firefox addon that adds a predictive gitmoji picker to GitHub commit message inputs',
-				link: 'https://github.com/malted/githubmoji',
 			},
 		],
 	},
@@ -81,9 +75,14 @@ const tools = [
 		title: 'Editors and IDEs',
 		items: [
 			{
-				name: 'gitmoji-vscode',
-				description: 'Gitmoji tool for git commit messages in VS Code',
-				link: 'https://github.com/seatonjiang/gitmoji-vscode',
+				name: 'gitmoji-atom',
+				description: 'Gitmoji for Atom',
+				link: 'https://github.com/ThatXliner/gitmoji-atom',
+			},
+			{
+				name: 'Gitmoji Commit',
+				description: 'VS Code extension for composing Gitmoji commit messages',
+				link: 'https://github.com/benjaminadk/emojigit',
 			},
 			{
 				name: 'gitmoji-intellij-plugin',
@@ -91,29 +90,24 @@ const tools = [
 				link: 'https://plugins.jetbrains.com/plugin/12383-gitmoji-plus-commit-button',
 			},
 			{
-				name: 'gitmoji-sublimetext',
-				description: 'A Sublime Text plugin to add emojis in git commit messages',
-				link: 'https://packagecontrol.io/packages/Gitmoji',
-			},
-			{
-				name: 'gitmoji-atom',
-				description: 'Gitmoji for Atom',
-				link: 'https://github.com/ThatXliner/gitmoji-atom',
-			},
-			{
 				name: 'gitmoji.nvim',
 				description: 'Gitmojis for Neovim using nvim-cmp',
 				link: 'https://github.com/Dynge/gitmoji.nvim',
 			},
 			{
+				name: 'gitmoji-sublimetext',
+				description: 'A Sublime Text plugin to add emojis in git commit messages',
+				link: 'https://packagecontrol.io/packages/Gitmoji',
+			},
+			{
+				name: 'gitmoji-vscode',
+				description: 'Gitmoji tool for git commit messages in VS Code',
+				link: 'https://github.com/seatonjiang/gitmoji-vscode',
+			},
+			{
 				name: 'Neo gitmoji',
 				description: 'Telescope integration for gitmoji (NeoVim)',
 				link: 'https:https://github.com/HenriqueArtur/neo-gitmoji.nvim/',
-			},
-			{
-				name: 'Gitmoji Commit',
-				description: 'VS Code extension for composing Gitmoji commit messages',
-				link: 'https://github.com/benjaminadk/emojigit',
 			},
 			{
 				name: 'telescope-gitmoji.nvim',
@@ -155,14 +149,14 @@ const tools = [
 		title: 'Learn',
 		items: [
 			{
-				name: 'gitmemoji',
-				description: 'A game to learn gitmojis',
-				link: 'https://github.com/ImBIOS/gitmemoji/',
-			},
-			{
 				name: 'Anki deck',
 				description: 'Learn gitmojis using Anki, a spaced repetition software',
 				link: 'https://ankiweb.net/shared/info/813391103',
+			},
+			{
+				name: 'gitmemoji',
+				description: 'A game to learn gitmojis',
+				link: 'https://github.com/ImBIOS/gitmemoji/',
 			},
 		],
 	},
@@ -171,27 +165,18 @@ const tools = [
 		title: 'Programming language integration',
 		items: [
 			{
-				name: 'gitmoji-regex',
-				description: 'A Gitmoji::Regex for Ruby',
-				link: 'https://github.com/pboling/gitmoji-regex',
-			},
-			{
 				name: 'convert-gitmoji',
 				description: 'Simple way to replace strings with gitmojis in TypeScript',
 				link: 'https://github.com/Intevel/convert-gitmoji',
 			},
+			{
+				name: 'gitmoji-regex',
+				description: 'A Gitmoji::Regex for Ruby',
+				link: 'https://github.com/pboling/gitmoji-regex',
+			},
 		],
 	},
 ]
-
-const shuffle = ([...arr]) => {
-  let m = arr.length;
-  while (m) {
-    const i = Math.floor(Math.random() * m--);
-    [arr[m], arr[i]] = [arr[i], arr[m]];
-  }
-  return arr;
-};
 
 const RelatedTools = () => (
   <>
@@ -205,11 +190,11 @@ const RelatedTools = () => (
           This is a list of tools which are related with the <b>gitmoji</b>{' '}
           convention.
         </p>
-				{shuffle(tools).map((section) => (
+				{tools.map((section) => (
 					<>
 						<h2>{section.emoji} {section.title}</h2>
 						<ul>
-							{shuffle(section.items).map((tool) => (
+							{section.items.map((tool) => (
 								<li key={tool.name} >
 									<a href={tool.link} target="_blank" rel="noopener noreferrer">
 										<b>{tool.name}</b>
